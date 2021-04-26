b4cb048c82f2424412c8845dbfc7819ffc2df70d
Brichard.Z <brichard@web-premiere.fr>
2021-04-26 18:33:03 +0200

    [Version 1] Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+SendMail With Swift Use Twig Template
+=====================================
+This Library is using for Symfony Version 4 or higher 
+
+Installation
+------------
+
+Clone or download it , place in vendor path 
+
+Usage
+-----
+
+Configure in your service setting ,make class : [``LCGLib\SrvSendMail`` class](src/LCG/SrvSendMail.php)
---
+++ src/LCG/SrvSendMail.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm .
+ * Date: 26/04/2021
+ * Time: 18:05
+ */
+
+namespace LCGLib;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class Mail
+ * @package LCGLib
+ * @author  Brichard.Z<brichard.zafy@gmail.com>
+ */
+class SrvSendMail
+{
+    /**
+     * @var \Swift_Mailer
+     */
+    private $_mailer;
+    /**
+     * @var \Symfony\Bridge\Twig\Form\TwigRendererInterface
+     */
+    private $_templating;
+
+    private $_sender = "";
+    /**
+     * @var string
+     */
+    private $_app_name = "";
+
+    /**
+     * SrvSendMail constructor.
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     * @param                                                           $app_name
+     */
+    public function __construct(ContainerInterface $container,$app_name)
+    {
+        $transport = (new Swift_SmtpTransport(
+            $container->getParameter("mailer_transport"),
+            $container->getParameter("mailer_port")))
+            ->setUsername($container->getParameter("mailer_email"))
+            ->setPassword($container->getParameter("mailer_password")) ;
+        $this->_mailer  = new Swift_Mailer($transport);
+        $this->_templating = $container->get('twig');
+        $this->_app_name   = $app_name;
+        $this->_sender     = $container->getParameter("mailer_user");
+    }
+
+    /**
+     * @param        $to
+     * @param string $subject
+     * @param string $template_name
+     * @param array  $message_vars
+     * @param array  $attachments
+     * @return bool
+     * @throws \Exception
+     */
+    public function send($to, string $subject, string $template_path_name,
+                         array $message_vars=[],
+                         $attachments = []): bool
+    {
+        $message = (new \Swift_Message($subject))
+            ->setFrom($this->getSender(),$this->_app_name)
+            ->setTo($to)
+            ->setBody(
+                $this->_templating->render(
+                    $template_path_name.'.html.twig',
+                    $message_vars
+                ),
+                'text/html'
+            );
+        if(count($attachments)>0){
+            foreach ($attachments as $attachment) {
+                $message->attach(\Swift_Attachment::fromPath($attachment));
+            }
+        }
+        $fail_recipient = [];
+        $res= $this->_mailer->send($message,$fail_recipient);
+        if(!empty($fail_recipient)){
+            print_r($fail_recipient);
+            throw new \Exception("Recipient not correct ".print_r($fail_recipient,true));
+        }
+        return $res;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSender()
+    {
+        return $this->_sender;
+    }
+
+    /**
+     * @param mixed $sender
+     * @return \LCGLib\SrvSendMail
+     */
+    public function setSender($sender)
+    {
+        $this->_sender = $sender;
+        return $this;
+    }
+}
